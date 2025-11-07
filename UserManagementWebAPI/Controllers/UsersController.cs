1a9b0bd06cd65f8c56834e5c5b108555f87f5d5a
Mahla <motieeatefeh@gmail.com>
2025-11-07 18:30:30 +0330

    #"Add new project changes"

---
+++ UserManagementWebAPI/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagementWebAPI.Service;
+using UserManagementWebAPI.Model;
+using System.Reflection.Metadata.Ecma335;
+
+namespace UserManagementWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] User user)
+        {
+            _userService.AddUser(user);
+            return Ok(user);
+        }
+        [HttpGet("byid/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if (user == null)
+
+                return NotFound();
+
+            return Ok(user);
+
+
+        }
+        [HttpGet("byemail/{email}")]
+        public IActionResult GetByEmail(string email)
+        {
+            var user = _userService.GetUserByEmail(email);
+            if (user == null)
+
+                return NotFound();
+
+            return Ok(user);
+        }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] User login)
+        {
+            var result = _userService.Login(login.Email, login.Password);
+            if (!result) return Unauthorized();
+            else return Ok("Login successfully!");
+
+        }
+    }
+}
---
+++ UserManagementWebAPI/Service/UserService.cs
@@ -0,0 +1,36 @@
+﻿using UserManagementWebAPI.Model;
+
+namespace UserManagementWebAPI.Service
+{
+    public class UserService
+    {
+        List<User> li = new List<User>();
+
+        public void AddUser(User user)
+        {
+            user.Id = new Random().Next(1,90000);
+            li.Add(user);
+
+        }
+        public User GetUserById(int id)
+        {
+            var result = li.FirstOrDefault(x => x.Id == id);
+            return result;
+
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            var result = li.FirstOrDefault(x => x.Email == email);
+            return result;
+        }
+
+        public bool Login(string email, string password)
+        {
+            return li.Any(x => x.Email == email && x.Password == password);
+
+        }
+
+
+    }
+}
